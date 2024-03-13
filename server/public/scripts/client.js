61740944db9f2b23ad6aa116e5b7fc3ab41fcef2
Moha Ali <Moha.gosaar@gmail.com>
2024-03-13 10:27:04 -0500

    Done koala-Holla

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,31 @@
-console.log( 'js' );
+console.log("js");
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  axios
+    .get("/koala")
+    .then((response) => {
+      saveKoala(response.data);
+      console.log("in getKoalas");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   // axios call to server to get koalas
-  
-} // end getKoalas
+}
 
-function saveKoala(){
-  console.log( 'in saveKoala' );
-  // axios call to server to get koalas
- 
+function saveKoala(koalas) {
+  let table = document.getElementById("displaydata");
+  koalas.forEach((koala) => {
+    table.innerHTML += `<tr>
+                          <td>${koala.id}</td>
+                          <td>${koala.name}</td>
+                          <td>${koala.favorite_color}</td>
+                          <td>${koala.age}</td>
+                          <td>${koala.ready_to_transer}</td>
+                          <td>${koala.notes}</td>
+                        </tr>`;
+  });
+  console.log("in saveKoala");
 }
 
 getKoalas();
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,17 +1,66 @@
-const express = require('express');
+const express = require("express");
 const koalaRouter = express.Router();
-
+let koalaHolla = [
+  {
+    id: 1,
+    name: "Scotty",
+    favorite_color: " Red ",
+    age: 4,
+    ready_to_transer: "Y",
+    notes: "Born in Guatemala ",
+  },
+  {
+    id: 2,
+    name: "Jean ",
+    favorite_color: " Green  ",
+    age: 5,
+    ready_to_transer: "Y",
+    notes: "Allergic to lots of lava",
+  },
+  {
+    id: 3,
+    name: "Ororo  ",
+    favorite_color: "Yellow",
+    age: 7,
+    ready_to_transer: "N",
+    notes: " Loves listening to Paula (Abdul)",
+  },
+  {
+    id: 4,
+    name: " K'Leaf",
+    favorite_color: " Purple  ",
+    age: 15,
+    ready_to_transer: " N",
+    notes: " Never refuses a treat",
+  },
+  {
+    id: 5,
+    name: "Charlie",
+    favorite_color: "Orange",
+    age: 9,
+    ready_to_transer: "Y",
+    notes: "Favorite band is Nirvana ",
+  },
+  {
+    id: 6,
+    name: "Betsy ",
+    favorite_color: " Blue ",
+    age: 4,
+    ready_to_transer: "Y",
+    notes: " Has a pet iguana",
+  },
+];
 // DB CONNECTION
 
-
 // GET
-
+koalaRouter.get("/", (request, response) => {
+  response.json(koalaHolla);
+  console.log(request.query);
+});
 
 // POST
 
-
 // PUT
-
 
 // DELETE
 
